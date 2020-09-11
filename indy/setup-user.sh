b3f789f93b41ee3f2af307f27114acac3c60f31d
Jeremy Whiting <jwhiting@redhat.com>
2020-09-11 12:13:05 +0100

    Change permission on .profile Bourne shell config.

--- indy/setup-user.sh
+++ indy/setup-user.sh
@@ -3,6 +3,7 @@ echo "Generate passwd file based on dynamic user for containers in Openshift and
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 envsubst < /opt/passwd.template > $HOME/passwd
+chmod 744 $HOME/passwd
 export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=$HOME/passwd
 export NSS_WRAPPER_GROUP=/etc/group
